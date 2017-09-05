2840fb29b915daf3f16cda1f94c5163989231cee
Pine Mizune <pinemz@gmail.com>
2017-09-05 21:19:06 +0900

    fix(InviteController): add error handling

--- app/controllers/InviteController.scala
+++ app/controllers/InviteController.scala
@@ -2,14 +2,14 @@ package controllers
 
 import javax.inject._
 
+import dispatch.Defaults._
 import dispatch._
-import Defaults._
 import models._
 import org.json4s.JsonAST.JObject
 import play.api.Logger
 import play.api.data.Forms._
 import play.api.data._
-import play.api.libs.json._
+import play.api.libs.json.Json
 import play.api.mvc._
 
 import scala.concurrent.Future
@@ -23,31 +23,43 @@ class InviteController @Inject()(cc: ControllerComponents) extends AbstractContr
   )
 
   def invite = Action.async { implicit request =>
-    val token = System.getenv("SLACK_API_TOKEN")
-    val team = System.getenv("SLACK_TEAM")
-
-    inviteForm.bindFromRequest.fold(
-      _ => Future.successful(BadRequest),
-      inviteData => {
-        Logger.debug(s"team:$team email:${inviteData.email}")
-
-        val req = url(s"https://$team.slack.com/api/users.admin.invite")
-          .addQueryParameter("token", token)
-          .addQueryParameter("email", inviteData.email)
-          .addQueryParameter("set_active", "true")
-          .POST
-
-        for {
-          res <- Http.default(req OK as.json4s.Json)
-          result <- {
-            val obj = res.asInstanceOf[JObject].values
-            val ok = obj.get("ok").asInstanceOf[Option[Boolean]]
-            val error = obj.get("error").asInstanceOf[Option[String]]
-            val result = InviteResponse(ok.getOrElse(false), error)
-            Future.successful(Ok(Json.toJson(result)))
-          }
-        } yield result
+    (
+      System.getenv("SLACK_API_TOKEN"),
+      System.getenv("SLACK_TEAM")
+    ) match {
+      case (null | "", _) => {
+        val result = InviteResponse(ok = false, Some("invalid_api_token"))
+        Future.successful(BadRequest(Json.toJson(result)))
       }
-    )
+      case (_, null | "") => {
+        val result = InviteResponse(ok = false, Some("invalid_team"))
+        Future.successful(BadRequest(Json.toJson(result)))
+      }
+      case (token, team) => {
+        inviteForm.bindFromRequest.fold(
+          _ => Future.successful(BadRequest),
+          inviteData => {
+            Logger.debug(s"team:$team email:${inviteData.email}")
+
+            val req = url(s"https://$team.slack.com/api/users.admin.invite")
+              .addQueryParameter("token", token)
+              .addQueryParameter("email", inviteData.email)
+              .addQueryParameter("set_active", "true")
+              .POST
+
+            for {
+              res <- Http.default(req OK as.json4s.Json)
+              result <- {
+                val obj = res.asInstanceOf[JObject].values
+                val ok = obj.get("ok").asInstanceOf[Option[Boolean]]
+                val error = obj.get("error").asInstanceOf[Option[String]]
+                val result = InviteResponse(ok.getOrElse(false), error)
+                Future.successful(Ok(Json.toJson(result)))
+              }
+            } yield result
+          }
+        )
+      }
+    }
   }
 }
